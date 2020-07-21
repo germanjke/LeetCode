e54af60ea2cbbe3cf0a91275c74582367f1b7a1b
German Abramov <germanjke@gmail.com>
2020-07-21 22:15:36 +0300

    Update 1365_for.cpp

--- c++/1365. How Many Numbers Are Smaller Than the Current Number/1365_for.cpp
+++ c++/1365. How Many Numbers Are Smaller Than the Current Number/1365_for.cpp
@@ -5,9 +5,9 @@ public:
         for (int i=0;i<nums.size();i++) //проверяю по индексам из nums
         {
             int count = 0; //завожу переменную которая будет считать сколько элементов больше
-            for (int num=0;num<nums.size();num++) //теперь делаю for loop в самих nums по значениям
+            for (auto x : nums) //теперь делаю for loop в самих nums по значениям
             {
-                if (nums[i]>num) //если nums с индексом i больше чем значение num из nums
+                if (nums[i]>x) //если nums с индексом i больше чем значение num из nums
                     count += 1; //увеличим на единичку наш count
             }
             res.push_back(count); //в конце первого лупа мы добавим этот каунт в наш res
