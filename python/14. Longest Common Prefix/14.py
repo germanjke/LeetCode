73492e02213d04930ab810af031b81907f326570
German Abramov <germanjke@gmail.com>
2020-08-14 13:03:00 +0300

    Update 14.py

--- python/14. Longest Common Prefix/14.py
+++ python/14. Longest Common Prefix/14.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ans = ''
-        if strs == []:
+        ans = '' #результат 
+        if strs == []: #если пуст
             return ans
-        for i in range(len(min(strs))):
-            c = strs[0][i]
-            for s in strs:
-                if s[i] != c:
+        for i in range(len(min(strs))): #проходимся по всем индексам самого маленького слова
+            c = strs[0][i] #первое слово, индексы букв
+            for s in strs: #остальные слова
+                if s[i] != c: #если инедекс всех слов не равно индексу первого слова
                     return ans
-            ans += c
+            ans += c #если равно то добавляем c
         return ans
     
                     
