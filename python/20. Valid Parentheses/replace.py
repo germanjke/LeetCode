5270817dcc72627660d6da9695cf62a1e91e6a58
German Abramov <germanjke@gmail.com>
2020-09-20 00:00:14 +0300

    Update replace.py

--- python/20. Valid Parentheses/replace.py
+++ python/20. Valid Parentheses/replace.py
@@ -3,3 +3,5 @@
         while '[]' in s or '()' in s or '{}' in s:
             s = s.replace('[]','').replace('()','').replace('{}','')
         return not len(s)
+
+   #UPD TIME LIMIT!!!! RLY BAD TIME! ITS O(N)^2
