89330d5735661484658e27051fd4d08238be238d
German Abramov <germanjke@gmail.com>
2020-09-27 23:48:32 +0300

    Create 232.py

---
+++ python/232. Implement Queue using Stacks/232.py
@@ -0,0 +1,47 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.array = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.array.append(x)
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        res = self.array[0]
+        self.array.pop(0)
+        return res
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.array[0]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.array) == 0:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
