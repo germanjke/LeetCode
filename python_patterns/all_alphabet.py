4008d56eadce6c62a6d925774f1338f430c40751
German Abramov <germanjke@gmail.com>
2020-11-04 18:33:09 +0300

    Update all_alphabet.py

--- python_patterns/all_alphabet.py
+++ python_patterns/all_alphabet.py
@@ -1,5 +1,9 @@
-alphabet = [chr(x) for x in range (65,123)]
+alphabet_u = [chr(x) for x in range (65,91)] # upper case 
 
-alphabet = list(map(chr, range(65, 123)))
+alphabet_l = list(map(chr, range(97, 123))) #lower case
 
-#97 from lower case
+alphabet_u = [chr(i) for i in range(ord('A'),ord('Z')+1)]
+
+alphabet_l = [chr(i) for i in range(ord('a'),ord('z')+1)]
+
+alphabet_u + alphabet_l
